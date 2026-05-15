88e15c3f62705b880930df20f5f87966dbda5fb3
umayer10-AI <mdumayer10@gmail.com>
2026-05-15 10:23:46 +0600

    add

--- server/index.js
+++ server/index.js
@@ -21,7 +21,13 @@ const client = new MongoClient(uri, {
 
 const verify = async (req,res,next) => {
     const header = req.headers.authorization
+    if(!header){
+        return req.status(401).json({message: "Unauthorized"})
+    }
     const token = header.split(' ')[1]
+    if(!token){
+        return req.status(401).json({message: "Unauthorized"})
+    }
     console.log(token)
     next()
 }
